a033b6369cddc143a655aa98939df631e48a5798
Gokul <gokulyc@gmail.com>
2021-02-01 19:39:10 +0530

    Rendering optimiztions
    Known Issue : OrderSummary Alert Update issue

--- my-app/src/components/Burger/OrderSummary/OrderSummary.js
+++ my-app/src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,24 +1,42 @@
-import React from "react";
+import React, { Component } from "react";
 import HDiv from "../../../hoc/HDiv";
 import lodash from "lodash";
 import Button from "../../UI/Button/Button";
 
-const orderSummary=(props)=>{
-    const ingredientSummary=lodash.keys(props.ingredients).map( igKey=>{
-        return <li key={igKey}><span style={{textTransform:'capitalize'}}>{igKey}</span>: {props.ingredients[igKey]}</li>
-    });
-    return (<HDiv>
-        <h3>
-            Your Order
-        </h3>
+class OrderSummary extends Component {
+  componentWillUpdate() {
+    console.log("OrderSummary updates");
+  }
+
+  render() {
+    const ingredientSummary = lodash
+      .keys(this.props.ingredients)
+      .map((igKey) => {
+        return (
+          <li key={igKey}>
+            <span style={{ textTransform: "capitalize" }}>{igKey}</span>:{" "}
+            {this.props.ingredients[igKey]}
+          </li>
+        );
+      });
+    return (
+      <HDiv>
+        <h3>Your Order</h3>
         <p>A delicious burger with following ingredients: </p>
-        <ul>
-            {ingredientSummary}
-        </ul>
-        <p><strong>Total Price : {props.price}</strong></p>
+        <ul>{ingredientSummary}</ul>
+        <p>
+          <strong>Total Price : {this.props.price}</strong>
+        </p>
         <p>Continue to Checkout ? </p>
-        <Button btnType="Danger" clicked={props.purchaseCancelled}>Cancel</Button>
-        <Button btnType="Success" clicked={props.purchaseContinued}>Continue</Button>
-    </HDiv>);
-};
-export default orderSummary;
+        <Button btnType="Danger" clicked={this.props.purchaseCancelled}>
+          Cancel
+        </Button>
+        <Button btnType="Success" clicked={this.props.purchaseContinued}>
+          Continue
+        </Button>
+      </HDiv>
+    );
+  }
+}
+
+export default OrderSummary;
--- my-app/src/components/UI/Modal/Modal.js
+++ my-app/src/components/UI/Modal/Modal.js
@@ -1,20 +1,34 @@
-import React from "react";
+import React, { Component } from "react";
 import classes from "./Modal.css";
 import HDiv from "../../../hoc/HDiv";
 import Backdrop from "../Backdrop/Backdrop";
 
-const modal = (props) => {
-    return (<HDiv>
-    <Backdrop show={props.show} clicked={props.modalClosed}/>
-    <div
-      className={classes.Modal}
-      style={{
-        transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-        opacity: props.show ? "1" : "0",
-      }}
-    >
-      {props.children}
-    </div>
-  </HDiv>);
+class Modal extends Component {
+  shouldComponentUpdate(nextProps, nextState) {
+    // return nextProps.show !== this.props.show;
+    // return nextProps.show !== this.props.show || nextProps.modalClosed !==this.props.modalClosed;
+    return true;
+
+  }
+  componentWillUpdate(){
+    console.log("Modal updates");
+  }
+  render() {
+    return (
+      <HDiv>
+        <Backdrop show={this.props.show} clicked={this.props.modalClosed} />
+        <div
+          className={classes.Modal}
+          style={{
+            transform: this.props.show ? "translateY(0)" : "translateY(-100vh)",
+            opacity: this.props.show ? "1" : "0",
+          }}
+        >
+          {this.props.children}
+        </div>
+      </HDiv>
+    );
+  }
 }
-export default modal;
+
+export default Modal;
