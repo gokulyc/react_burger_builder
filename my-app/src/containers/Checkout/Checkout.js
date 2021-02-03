063c14a9740d38d106f25ec036e30be2e489cff7
Gokul <gokulyc@gmail.com>
2021-02-03 13:27:04 +0530

    Fixed Navigation || Added Order Page and past orders feature || Complete Routing

--- my-app/src/containers/Checkout/Checkout.js
+++ my-app/src/containers/Checkout/Checkout.js
@@ -37,7 +37,7 @@ class Checkout extends Component{
             CheckoutCancelled={this.checkoutCancelledHandler}
             CheckoutContinued={this.checkoutContinuedHandler}
             ></CheckoutSummary>
-            //props for history obj
+            {/* //props for history obj */}
             <Route path={this.props.match.path+'/contact-data'}  render={(props) => (<ContactData ingredients={this.state.ingredients} price={this.state.totalPrice} {...props} />)}/>  
             
         </div>);
