003c27e11476573287050717c7952df9b181c896
Gokul Y <gokulyc@gmail.com>
2021-02-24 19:36:42 +0530

    Handling Purchases & Updating the UI KnownIssue: OderNow not working as expected

--- my-app/src/containers/Checkout/Checkout.js
+++ my-app/src/containers/Checkout/Checkout.js
@@ -1,35 +1,53 @@
 import React, { Component } from "react";
 import CheckoutSummary from "../../components/Order/CheckoutSummary/CheckoutSummary";
-import { Route } from "react-router-dom";
+import { Route, Redirect } from "react-router-dom";
 import ContactData from "./ContactData/ContactData";
 import { connect } from "react-redux";
+import * as actions from "../../store/actions/index";
 class Checkout extends Component {
-  checkoutCancelledHandler = () => {
-    this.props.history.goBack();
-  };
-  checkoutContinuedHandler = () => {
-    this.props.history.replace("/checkout/contact-data");
-  };
-  render() {
-    return (
-      <div>
-        <CheckoutSummary
-          ingredients={this.props.ings}
-          CheckoutCancelled={this.checkoutCancelledHandler}
-          CheckoutContinued={this.checkoutContinuedHandler}
-        ></CheckoutSummary>
-        {/* //props for history obj */}
-        <Route
-          path={this.props.match.path + "/contact-data"}
-          component={ContactData}
-        />
-      </div>
-    );
-  }
+    componentDidMount() {
+        this.props.onInitPurchase();
+    }
+    checkoutCancelledHandler = () => {
+        this.props.history.goBack();
+    };
+    checkoutContinuedHandler = () => {
+        this.props.history.replace("/checkout/contact-data");
+    };
+    render() {
+        let summary = <Redirect to="/" />;
+        if (this.props.ings) {
+            const purchasedRedirect = this.props.purchased ? (
+                <Redirect to="/" />
+            ) : null;
+            summary = (
+                <div>
+                    {purchasedRedirect}
+                    <CheckoutSummary
+                        ingredients={this.props.ings}
+                        CheckoutCancelled={this.checkoutCancelledHandler}
+                        CheckoutContinued={this.checkoutContinuedHandler}
+                    ></CheckoutSummary>
+                    {/* //props for history obj */}
+                    <Route
+                        path={this.props.match.path + "/contact-data"}
+                        component={ContactData}
+                    />
+                </div>
+            );
+        }
+        return summary;
+    }
 }
 const mapStateToProps = (state) => {
-  return {
-    ings: state.ingredients,
-  };
+    return {
+        ings: state.burgerBuilder.ingredients,
+        purchased: state.order.purchased,
+    };
 };
-export default connect(mapStateToProps)(Checkout);
+const mapDispatchToProps = (dispatch) => {
+    return {
+        onInitPurchase: () => dispatch(actions.purchaseInit()),
+    };
+};
+export default connect(mapStateToProps,mapDispatchToProps)(Checkout);
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -8,22 +8,29 @@ import registerServiceWorker from "./registerServiceWorker";
 //redux
 import { Provider } from "react-redux";
 import burgerBuilderReducer from "./store/reducers/burgerBuilder";
-import { createStore, applyMiddleware, compose } from "redux";
+import { createStore, applyMiddleware, compose, combineReducers } from "redux";
 import thunk from "redux-thunk";
+import orderReducer from "./store/reducers/order";
+// import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const rootReducer = combineReducers({
+    burgerBuilder: burgerBuilderReducer,
+    order: orderReducer,
+});
+
 const store = createStore(
-  burgerBuilderReducer,
-  composeEnhancers(applyMiddleware(thunk))
+    rootReducer,
+    composeEnhancers(applyMiddleware(thunk))
 );
 
-
 const app = (
-  <Provider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </Provider>
+    <Provider store={store}>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </Provider>
 );
 
 ReactDOM.render(app, document.getElementById("root"));
