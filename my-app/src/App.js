7e80c1e84cb86bcba323de88350430bd6c501e58
Gokul Y <gokulyc@gmail.com>
2021-02-26 14:03:24 +0530

    fixed auth localstorage and added user filter to Orders & Guarding Routes

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Route, Switch,withRouter } from "react-router-dom";
+import { Route, Switch, withRouter, Redirect } from "react-router-dom";
 import Layout from "./components/Layout/Layout";
 import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
 import Checkout from "./containers/Checkout/Checkout";
@@ -7,37 +7,49 @@ import Orders from "./containers/Orders/Orders";
 import Auth from "./containers/Auth/Auth";
 import Logout from "./containers/Auth/Logout/Logout";
 //redux
-import * as actions from './store/actions/index';
+import * as actions from "./store/actions/index";
 import { connect } from "react-redux";
 
-
 class App extends Component {
+    componentDidMount () {
+        this.props.onTryAutoSignup();
+      }
     render() {
+        let routes = (
+            <Switch>
+                <Route path="/auth" component={Auth} />
+                <Route path="/" exact component={BurgerBuilder} />
+                <Redirect to="/" />
+            </Switch>
+        );
+        if (this.props.isAuthenticated) {
+            routes = (
+                <Switch>
+                    <Route path="/logout" component={Logout} />
+                    <Route path="/checkout" component={Checkout} />
+                    <Route path="/orders" component={Orders} />
+                    <Route path="/" exact component={BurgerBuilder} />
+                    <Redirect to="/" />
+                </Switch>
+            );
+        }
         return (
             <div>
-                <Layout>
-                    <Switch>
-                        <Route path="/logout" component={Logout} />
-                        <Route path="/checkout" component={Checkout} />
-                        <Route path="/orders" component={Orders} />
-                        <Route path="/auth" component={Auth} />
-                        <Route path="/" exact component={BurgerBuilder} />
-                    </Switch>
-                </Layout>
+                <Layout>{routes}</Layout>
             </div>
         );
     }
 }
 
-const mapStateToProps = state => {
+const mapStateToProps = (state) => {
     return {
-      isAuthenticated: state.auth.token !== null
+        isAuthenticated: state.auth.token !== null,
     };
-  };
-  
-  const mapDispatchToProps = dispatch => {
+};
+
+const mapDispatchToProps = (dispatch) => {
     return {
-      onTryAutoSignup: () => dispatch( actions.authCheckStatus() )
+        onTryAutoSignup: () => dispatch(actions.authCheckStatus()),
     };
-  };
-export default withRouter(connect(mapStateToProps,mapDispatchToProps)(App));
+};
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(App));
--- my-app/src/containers/Orders/Orders.js
+++ my-app/src/containers/Orders/Orders.js
@@ -10,7 +10,7 @@ import CircularProgress from "@material-ui/core/CircularProgress";
 
 class Orders extends Component {
     componentDidMount() {
-        this.props.onFetchOrders(this.props.token);
+        this.props.onFetchOrders(this.props.token,this.props.userId);
     }
 
     render() {
@@ -33,11 +33,13 @@ const mapStateToProps = (state) => {
         orders: state.order.orders,
         loading: state.order.loading,
         token: state.auth.token,
+        userId: state.auth.userId,
     };
 };
 const mapDispatchToProps = (dispatch) => {
     return {
-        onFetchOrders: (token) => dispatch(actions.fetchOrders(token)),
+        onFetchOrders: (token, userId) =>
+            dispatch(actions.fetchOrders(token, userId)),
     };
 };
 
