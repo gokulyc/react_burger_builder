72e54dede6c46677038981cb5ddc5336415c6224
Gokul Y <gokulyc@gmail.com>
2021-03-02 12:16:41 +0530

    fixed css modules issue & fix ingredients load issue & dev redux issue

--- react-hook-main/src/App.js
+++ react-hook-main/src/App.js
@@ -1,18 +1,72 @@
-import './App.css';
-import React, { useContext } from 'react';
-import Ingredients from './components/Ingredients/Ingredients';
-import Auth from './components/Auth';
-import { AuthContext } from './context/auth-context';
+import "./App.css";
+import React, { Component } from "react";
+import { Route, Switch, withRouter, Redirect } from "react-router-dom";
+import asyncComponent from "./hoc/asyncComponent/asyncComponent";
+import Layout from "./hoc/Layout/Layout";
+import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
+import Logout from "./containers/Auth/Logout/Logout";
 
-const App = props => {
-  const authContext = useContext(AuthContext);
+// redux
+import { connect } from "react-redux";
+import * as actions from "./store/actions/index";
 
-  let content = <Auth />;
-  if (authContext.isAuth) {
-    content = <Ingredients />;
-  }
+const asyncCheckout = asyncComponent(() => {
+    return import("./containers/Checkout/Checkout");
+});
 
-  return content;
+const asyncOrders = asyncComponent(() => {
+    return import("./containers/Orders/Orders");
+});
+
+const asyncAuth = asyncComponent(() => {
+    return import("./containers/Auth/Auth");
+});
+
+class App extends Component {
+    componentDidMount() {
+        this.props.onTryAutoSignup();
+    }
+
+    render() {
+        let routes = (
+            <Switch>
+                <Route path="/auth" component={asyncAuth} />
+                <Route path="/" exact component={BurgerBuilder} />
+                <Redirect to="/" />
+            </Switch>
+        );
+
+        if (this.props.isAuthenticated) {
+            routes = (
+                <Switch>
+                    <Route path="/checkout" component={asyncCheckout} />
+                    <Route path="/orders" component={asyncOrders} />
+                    <Route path="/logout" component={Logout} />
+                    <Route path="/auth" component={asyncAuth} />
+                    <Route path="/" exact component={BurgerBuilder} />
+                    <Redirect to="/" />
+                </Switch>
+            );
+        }
+
+        return (
+            <div>
+                <Layout>{routes}</Layout>
+            </div>
+        );
+    }
+}
+
+const mapStateToProps = (state) => {
+    return {
+        isAuthenticated: state.auth.token !== null,
+    };
 };
 
-export default App;
+const mapDispatchToProps = (dispatch) => {
+    return {
+        onTryAutoSignup: () => dispatch(actions.authCheckState()),
+    };
+};
+
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(App));
--- react-hook-main/src/index.js
+++ react-hook-main/src/index.js
@@ -1,15 +1,42 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import AuthContextProvider from "./context/auth-context";
+
+
+//redux
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware, compose, combineReducers } from "redux";
+import thunk from "redux-thunk";
+import burgerBuilderReducer from "./store/reducers/burgerBuilder";
+import orderReducer from "./store/reducers/order";
+import authReducer from "./store/reducers/auth";
+
+const composeEnhancers =
+    (process.env.NODE_ENV === "development"
+        ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+        : null) || compose;
+
+const rootReducer = combineReducers({
+    burgerBuilder: burgerBuilderReducer,
+    order: orderReducer,
+    auth: authReducer,
+});
+
+const store = createStore(
+    rootReducer,
+    composeEnhancers(applyMiddleware(thunk))
+);
 
 ReactDOM.render(
     <React.StrictMode>
-        <AuthContextProvider>
-            <App />
-        </AuthContextProvider>
+        <Provider store={store}>
+            <BrowserRouter>
+                <App />
+            </BrowserRouter>
+        </Provider>
     </React.StrictMode>,
     document.getElementById("root")
 );
